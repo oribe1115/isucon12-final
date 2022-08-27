eb97474ecb404fa27bf768bc438bfd242a9c0066
motoki317 <motoki317@gmail.com>
2022-08-27 12:38:03 +0900

    one time token hard delete

--- webapp/sql/3_schema_exclude_user_presents.sql
+++ webapp/sql/3_schema_exclude_user_presents.sql
@@ -231,9 +231,8 @@ CREATE TABLE `user_one_time_tokens` (
   `created_at` bigint NOT NULL,
   `updated_at` bigint NOT NULL,
   `expired_at` bigint NOT NULL,
-  `deleted_at` bigint default NULL,
   PRIMARY KEY (`id`),
-  UNIQUE uniq_token (`user_id`, `token`, `deleted_at`)
+  UNIQUE uniq_token (`user_id`, `token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 /* 管理者権限のセッション管理 */
